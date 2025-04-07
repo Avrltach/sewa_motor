6d2d414100a7075d86818de5f507aab4aff9f910
hamka <hamkazainulardhi@gmail.com>
2025-04-07 07:55:31 +0700

    add delete and edit

--- delete_persediaan.php
+++ delete_persediaan.php
@@ -4,10 +4,10 @@ include_once("config.php");
 if (isset($_GET['id'])) {
     $id_motor = $_GET['id'];
 
-    // Hapus data dari tabel motor
+
     $result = mysqli_query($mysqli, "DELETE FROM motor WHERE id_motor=$id_motor");
 
-    // Jika berhasil, redirect ke dashboard dengan notifikasi
+
     if ($result) {
         header("Location: motor.php?hapus=berhasil");
         exit;
